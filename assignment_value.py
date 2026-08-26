7094596b462ef217f0abe000a29ab476a9e1c905
bhargavigajipara229-pixel <bhargavigajipara229@gmail.com>
2026-08-26 12:39:44 +0530

    Delete assignment_value.py

--- assignment_value.py
+++
@@ -1,9 +0,0 @@
-number = int(input("enter number") )
-number%=10
-print("%= 10 is {}".format(number))
-
-number//=10
-print("//=10 is {}".format(number))
-
-number**=10
-print("**=10 is {}".format(number))   
