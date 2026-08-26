b59d1db7670c67b6f71f9aa763708264d448e76c
bhargavigajipara229-pixel <bhargavigajipara229@gmail.com>
2026-08-26 12:44:31 +0530

    Delete sep_end.py

--- sep_end.py
+++
@@ -1,4 +0,0 @@
-print("Hello World","Bhargavi","ahemdabad",sep="\n")
-print("Hello World","Bhargavi","ahemdabad",sep="\t")
-print("Hello World","Bhargavi","ahemdabad",end="\n")
-print("Hello World","Bhargavi","ahemdabad",end="\t")
